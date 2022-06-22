3db39604f3a17f4f2d9fe0989f774e48ec785059
Moha <mohamedouikssani@gmail.com>
2022-06-22 13:04:44 +0200

    creamos servidor

---
+++ backend/servidor.js
@@ -0,0 +1,13 @@
+var express = require('express');       //para traer la libreria de express
+var app = express();                    //para crear una instancia de express
+
+
+app.get('/', home); //obtiene el home y ejecuta la funcion home
+
+
+function home(req, res) {
+    res.send('Hello World!');
+}
+
+
+app.listen(3000);
