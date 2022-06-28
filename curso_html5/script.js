8a354941d9d5a1e2f4db2152e898794db9c4d8c9
Moha <mohamedouikssani@gmail.com>
2022-06-29 01:46:14 +0200

    header mas chulo i fihado

---
+++ curso_html5/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("btn_ses").addEventListener("click", function(){
+    window.open("inicio_sesion.html", "_self");
+});
+
+document.getElementById("btn_reg").addEventListener("click", function(){
+    window.open("registro.html", "_self");
+});
+
+
+
