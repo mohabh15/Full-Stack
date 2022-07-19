b966b1ffa6af8d24acc6d8282b684ee23bbb1d6a
Moha <mohamedouikssani@gmail.com>
2022-07-19 14:29:57 +0200

    inicio js course

---
+++ Teoria/script.js
@@ -0,0 +1,44 @@
+/** 
+ * window.open("link") : open a new window with the link i devuelve la ventana que se abrio
+ * window.close() : close the current window
+ * window.closed() : verifica si esta cerrada o no 
+ * window.moveTo(x,y) : move the window to the x,y position
+ * window.resizeTo(width,height) : resize the window to the width,height
+ * window.print() : print the current window
+ * window.focus() : focus the current window
+ * window.blur() : blur the current window
+ * window.scroll(x,y) : scroll the window to the x,y position
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
